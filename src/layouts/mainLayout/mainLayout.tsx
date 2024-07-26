84cffc54d5100f56d038637af65d508a72c68196
Hinata0607 <atahin0607@gmail.com>
2024-07-26 21:05:18 +0900

    add: Left Bar Layout

--- src/layouts/mainLayout/mainLayout.tsx
+++ src/layouts/mainLayout/mainLayout.tsx
@@ -1,11 +1,24 @@
 import { MainLayoutProps } from '@/interfaces';
-import { MainHeader } from './block';
+import { LeftBar, MainHeader } from './block';
+import { Box } from '@mui/material';
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
 	return (
 		<>
 			<MainHeader />
-			{children}
+			<Box
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+				width="100%"
+				height="calc(100vh - 60px)"
+				bgcolor="#add"
+			>
+				<LeftBar />
+				<Box width="calc(100% - 250px)" height="100%">
+					{children}
+				</Box>
+			</Box>
 		</>
 	);
 };
