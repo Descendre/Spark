ba2d0da45c24eea43c5b5d1f03a4a93566fa80e0
Hinata0607 <atahin0607@gmail.com>
2024-07-26 20:49:08 +0900

    extend: MUI theme props

--- src/themes/darkTheme.ts
+++ src/themes/darkTheme.ts
@@ -4,6 +4,9 @@ import { createTheme } from '@mui/material';
 export const darkTheme = createTheme({
 	palette: {
 		mode: 'dark',
+		layout: {
+			primary: '#000000',
+		},
 		background: {
 			default: '#0d1318',
 		},
