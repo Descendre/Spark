5742f5116fa03e7c589feeb663729b7a172c6887
Hinata0607 <atahin0607@gmail.com>
2024-07-28 14:34:49 +0900

    add: MainHeader logo

---
+++ src/layouts/mainLayout/atom/MainHeaderLogo.tsx
@@ -0,0 +1,21 @@
+import { Avatar, Box } from '@mui/material';
+import React from 'react';
+
+export const MainHeaderLogo = () => {
+	return (
+		<Box
+			display="flex"
+			justifyContent="center"
+			alignItems="center"
+			height="100%"
+		>
+			<Avatar
+				src={'Spark_logo_transparent.svg'}
+				variant="square"
+				sx={{
+					width: 'auto',
+				}}
+			/>
+		</Box>
+	);
+};
